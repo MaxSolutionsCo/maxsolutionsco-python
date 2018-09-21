d0c25b81b0c8630e6f84cc1193f4ad92704f2ccf
José Hernández <jhbez@outlook.com>
2018-09-20 22:33:10 -0500

    add | endpoint api | chain, chain/tfd, chain/cfdi

--- innov/cfdi.py
+++ innov/cfdi.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# © Yonn, Xyz. All rights reserved.
 from .util import join_url
 from .api import default as default_api
 
@@ -20,5 +22,23 @@ class Cfdi:
     @classmethod
     def validate_uuid(cls, data, api=None):
         path = join_url(cls.path, 'validate/uuid')
+        api = api or default_api()
+        return api.post(path, data)
+
+    @classmethod
+    def chain(cls, data, api):
+        path = join_url(cls.path, 'chain')
+        api = api or default_api()
+        return api.post(path, data)
+
+    @classmethod
+    def chain_cfdi(cls, data, api):
+        path = join_url(cls.path, 'chain/cfdi')
+        api = api or default_api()
+        return api.post(path, data)
+
+    @classmethod
+    def chain_tfd(cls, data, api):
+        path = join_url(cls.path, 'chain/tfd')
         api = api or default_api()
         return api.post(path, data)
