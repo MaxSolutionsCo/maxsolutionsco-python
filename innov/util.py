9f1d7effbeaa1ce60b1a8ada7d754bb93d475c6f
José Hernández <jose.hbez@gmail.com>
2020-12-29 20:53:24 -0600

    fix - join url python3.7

--- innov/util.py
+++ innov/util.py
@@ -33,11 +33,10 @@ def join_url(url, *paths):
         >>> util.join_url('api.sandbox.innov.biz', '/v1','/cfdi/')
         'api.sandbox.innov.biz/v1/demo/'
     """
-    #if platform.python_version()[:3] == '3.7':
-    #    from urllib.parse import urljoin
-    #    for path in paths:
-    #        url = urljoin(url, path)
-    #    return url
+    if platform.python_version()[:3] == '3.7':
+        from urllib.parse import urljoin        
+        path =  "/".join(map(lambda x: str(x).strip('/'), paths))
+        return urljoin(url, path)
         
     for path in paths:
         url = re.sub(r'/?$', re.sub(r'^/?', '/', path), url)
